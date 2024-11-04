4190c0deec33bd65c17bdcd8d7eb3fc09d046161
ericl343 <ericl343@nycstudents.net>
2024-11-04 16:27:30 -0500

    draft

---
+++ Lin_Eric_Lecture_8.cpp
@@ -0,0 +1,32 @@
+//Eric Lin
+//November 4th, 2024
+//Lecture 8
+
+
+#include <iostream>  
+#include <cmath>        // Include for mathematical functions
+#include <cctype>       // Include for character handling functions
+#include <iomanip>      // Include for manipulators used to format output
+
+using namespace std;    // Makes all the names in the std namespace available
+
+int doubleMultiply(int a, int b)
+{
+    double flo;
+    int product;
+    product = a * b * 2;
+    return product;
+    double getSum;
+}
+int main()
+{
+    cin >> flo;
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    cout << "Twice the product of the number is: "
+        << doubleMultiply(num1, num2) << endl;
+    
+
+    return 0;
+}
