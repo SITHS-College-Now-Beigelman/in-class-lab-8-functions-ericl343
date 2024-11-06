25bbaf2c73e306fbc39815df2bb103a74c961074
ericl343 <ericl343@nycstudents.net>
2024-11-05 22:24:31 -0500

    1

--- Lin_Eric_Lecture_8.cpp
+++ Lin_Eric_Lecture_8.cpp
@@ -6,27 +6,61 @@
 #include <iostream>  
 #include <cmath>        // Include for mathematical functions
 #include <cctype>       // Include for character handling functions
-#include <iomanip>      // Include for manipulators used to format output
 
 using namespace std;    // Makes all the names in the std namespace available
 
-int doubleMultiply(int a, int b)
-{
-    double flo;
-    int product;
-    product = a * b * 2;
-    return product;
-    double getSum;
+//For Part 2
+int doubleMultiply(int a, int b) {
+    int pro;
+    pro = a * b * 2;
+    return pro; 
 }
-int main()
-{
-    cin >> flo;
-    int num1, num2;
-    cout << "Enter two integers: ";
-    cin >> num1 >> num2;
-    cout << "Twice the product of the number is: "
-        << doubleMultiply(num1, num2) << endl;
-    
+//For Part 3
+double getSum(double x, double y, double z) {
+    return x + y + z;
+}
 
-    return 0;
+//Part 1
+int main() {
+double num;
+cout << "Enter a floating point number: ";
+cin >> num;
+    
+//Printing results after calculating
+cout << "Square root of the number: " << sqrt(num) << endl;
+cout << "Number raised to the 4th power: " << pow(num, 4) << endl;
+cout << "Floor of the number: " << floor(num) << endl;
+
+/*
+Enter a floating point number: 4.5
+Square root of the number: 2.12132
+Number raised to the 4th power: 410.062
+Floor of the number: 4
+*/
+
+//Part 2
+int num1, num2;
+cout << "Enter two integers: ";
+cin >> num1 >> num2;
+cout << "Twice the product of the number is: "
+<< doubleMultiply(num1, num2) << endl;
+
+/*
+Enter two integers: 3 2
+Twice the product of the number is: 12
+*/
+    
+//Part 3
+double a, b, c;
+cout << "Enter three floating point numbers: ";
+cin >> a >> b >> c;
+double result = getSum(a, b, c);
+cout << "The sum of the numbers is: " << result << endl;
+
+/*
+Enter three floating point numbers: 1.6 5.8 3.7
+The sum of the numbers is: 11.1
+*/
+
+return 0;
 }
